7b8b40ac1d7b628125f67112c2bea810b31fcac7
Gao ZhiChao <60123894@qq.com>
2019-05-27 14:56:15 +0800

    同一浏览器打开两个标签页，vue store状态不同步，token不使用cookie存放，用store存放
    Signed-off-by: Gao ZhiChao <60123894@qq.com>

--- vue-dev-tool/src/store/cn/moonnow/sign/usr/usrStore.js
+++ vue-dev-tool/src/store/cn/moonnow/sign/usr/usrStore.js
@@ -3,11 +3,16 @@ import usrService from '@/api/cn/moonnow/sign/usr/usrService'
 export default {
   namespaced: true,
   state: {
+    aa: 'qwer'
   },
   mutations: {
+    setAa (state) {
+      state.aa = 'asdf'
+    }
   },
   actions: {
     cs ({ commit, state }) {
+      commit('setAa')
       return new Promise((resolve, reject) => {
         usrService.cs().then(res => {
           resolve(res)
